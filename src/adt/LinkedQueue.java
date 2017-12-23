5de46bd15a299581c6ae7974d73bab0e57b42847
USHA <USHA@Ash-PC>
2017-12-23 23:53:07 +0800

    Created LinkedQueue

---
+++ src/adt/LinkedQueue.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adt;
+
+import adt.*;
+import entity.Node;
+
+/**
+ *
+ * @author USHA
+ */
+public class LinkedQueue<T> implements QueueInterface<T> {
+
+    private Node topNode;
+    private Node bottomNode;
+    private int size;
+
+    public LinkedQueue() {
+        topNode = null;
+        bottomNode = null;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(T newEntry) {
+
+        Node newNode = new Node(newEntry);
+        if (isEmpty()) {
+            topNode = newNode;
+            bottomNode = newNode;
+            size++;
+        } else {
+            bottomNode.setNext(newNode);
+            bottomNode = newNode;
+            size++;
+        }
+    }
+
+    @Override
+    public T dequeue() {
+        Node node = topNode;
+        topNode = topNode.getNext();
+
+        return (T) node.getData();
+    }
+
+    @Override
+    public T getFront() {
+        return (T) topNode.getData();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        topNode = null;
+        bottomNode = null;
+        size = 0;
+    }
+
+    public String toString() {
+
+        String str = "";
+        Node node = topNode;
+
+        if (size > 0) {
+            while (node != null){
+                str += node.getData().toString() + "\n";
+                node = node.getNext();
+            }
+
+            
+        }
+        return str;
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+}
