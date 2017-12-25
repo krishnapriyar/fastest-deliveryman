80d0ff85c7bb57f2cd856e1baa707921a1dc692a
USHA <USHA@Ash-PC>
2017-12-25 16:31:46 +0800

    Refactor complete

--- src/ModuleB/adt/ListInterface.java
+++ src/ModuleB/adt/ListInterface.java
@@ -13,11 +13,17 @@ package ModuleB.adt;
 public interface ListInterface<T> {
     
     void add(T newEntry);
+    
     boolean remove(T anEntry);
+    
     T getEntry(int index);
+    
     boolean contains(T anEntry);
+    
     boolean isEmpty();
+    
     String toString();
+    
     int getSize();
     
 }
