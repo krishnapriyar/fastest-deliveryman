dceb9d2752c1fae7f0432d4a7091a79e94296b66
chong <chong@kunming>
2017-12-26 23:19:20 +0800

    Update Module

--- src/ModuleE/ui/CustomerMainMenuUI.java
+++ src/ModuleE/ui/CustomerMainMenuUI.java
@@ -27,31 +27,31 @@ public class CustomerMainMenuUI extends JFrame {
     private JPanel jpnPanel2 = new JPanel(new GridLayout(3, 2));
     private int custID;
     public ModuleE.entity.ListGetterSetter arrList = new ModuleE.entity.ListGetterSetter();
-    private static QueueInterface<OrderClass> orderList = new LinkedQueue<>(); 
-    
+    private static QueueInterface<OrderClass> orderList = new LinkedQueue<>();
+
     public CustomerMainMenuUI() {
-        
+
     }
-    
-    public static void setQueue(QueueInterface<OrderClass> orderqueue){
+
+    public static void setQueue(QueueInterface<OrderClass> orderqueue) {
         orderList = orderqueue;
     }
-    
-    public QueueInterface<OrderClass> getQueue(){
+
+    public QueueInterface<OrderClass> getQueue() {
         return orderList;
     }
-    
+
     public CustomerMainMenuUI(ModuleE.entity.ListGetterSetter arrClass, String username) {
         arrList = arrClass;
         getCustInfo(username);
-        
+
         JButton jbtMakeOrder = new JButton("Make order");
         JButton jbtMakeScheduleOrder = new JButton("Make Schedule Order");
         JButton jbtViewScheduledOrder = new JButton("View My Schedule Order");
         JButton jbtTrackOrder = new JButton("Track my order");
         JButton jbtLogout = new JButton("Logout");
         JLabel jlblTitle = new JLabel("Customer Main Menu");
-        
+
         java.awt.Font font = new java.awt.Font("Arial", java.awt.Font.PLAIN, 20);
         jlblTitle.setFont(font);
         jbtMakeOrder.setFont(font);
@@ -59,26 +59,25 @@ public class CustomerMainMenuUI extends JFrame {
         jbtViewScheduledOrder.setFont(font);
         jbtTrackOrder.setFont(font);
         jbtLogout.setFont(font);
-        
+
         jbtMakeOrder.addActionListener(new java.awt.event.ActionListener() {
-           
-                 public void actionPerformed(ActionEvent e) {
-               MakeOrder makeOrder = new MakeOrder(); 
-               makeOrder.orderPage(username, custID);
-               CustomerMainMenuUI.this.setVisible(false);
+
+            public void actionPerformed(ActionEvent e) {
+                MakeOrder makeOrder = new MakeOrder();
+                makeOrder.orderPage(username, custID);
+                CustomerMainMenuUI.this.setVisible(false);
             }
-            
+
         });
-        
+
         jbtViewScheduledOrder.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent e) {
                 ViewMyOrderUI view = new ViewMyOrderUI();
-                
                 view.pageContent(arrClass, custID, username);
                 CustomerMainMenuUI.this.setVisible(false);
             }
         });
-        
+
         jbtMakeScheduleOrder.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent e) {
                 SelectDateTimeUI makeBooking = new SelectDateTimeUI();
@@ -86,7 +85,7 @@ public class CustomerMainMenuUI extends JFrame {
                 CustomerMainMenuUI.this.setVisible(false);
             }
         });
-        
+
         jbtLogout.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent e) {
                 ui.LoginFrame login = new ui.LoginFrame();
@@ -95,7 +94,7 @@ public class CustomerMainMenuUI extends JFrame {
                 CustomerMainMenuUI.this.setVisible(false);
             }
         });
-        
+
         JPanel jpn3 = new JPanel();
         jpnPanel2.add(jbtMakeOrder);
         jpnPanel2.add(jbtMakeScheduleOrder);
@@ -103,39 +102,39 @@ public class CustomerMainMenuUI extends JFrame {
         jpnPanel2.add(jbtViewScheduledOrder);
         jpnPanel2.add(jbtLogout);
         jpn3.add(jpnPanel2);
-        
+
         add(jlblTitle, BorderLayout.NORTH);
         add(jpn3, BorderLayout.CENTER);
-        
+
         setTitle("Customer Main Menu");
         setSize(600, 300);
         setVisible(true);
         setLocationRelativeTo(null);
     }
-    
+
     private void getCustInfo(String username) {
         if (connection() == true) {
             try {
                 arrList.getCustList().clearList();
                 prepare = conn.prepareStatement("SELECT * FROM CUSTOMER");
                 rs = prepare.executeQuery();
-                
+
                 while (rs.next()) {
                     if (rs.getString(2).replace(" ", "").equalsIgnoreCase(username)) {
                         custID = rs.getInt(1);
                         arrList.getCustList().addNewItem(new ModuleE.entity.Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4)));
-                    }                    
+                    }
                 }
-                
+
             } catch (SQLException ex) {
             }
-            
+
         }
     }
-    
+
     private boolean connection() {
         boolean isSuccess = false;
-        
+
         try {
             conn = java.sql.DriverManager.getConnection(dbURL);
             if (conn != null) {
@@ -143,7 +142,7 @@ public class CustomerMainMenuUI extends JFrame {
             }
         } catch (SQLException ex) {
         }
-        
+
         return isSuccess;
     }
 }
